ec4ceac8bf7d08b5a74eeb561b9d3045c508f567
unknown <bjdenys@mtu.edu>
2016-09-11 10:32:20 -0400

    improved player tracking and added gitignore

--- Assets/Scripts/MobAI.cs
+++ Assets/Scripts/MobAI.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class MobAI : MonoBehaviour {
-    public GameObject Control;
+    public GameObject Player;
     int MinDist = 5;
-    float speed = 3;
+    float speed = 2;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,24 +12,30 @@ public class MobAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Debug.Log((transform.position.x) - (Player.transform.position.x));
+        Debug.Log((Player.transform.position.x) - (transform.position.x));
+        if (Mathf.Abs((transform.position.x) - (Player.transform.position.x)) > 1.25|| Mathf.Abs((Player.transform.position.x) -(transform.position.x)) > 1.25)
+        {
+            if (transform.position.x > Player.transform.position.x)
+            {
+                transform.Translate(Vector2.left * Time.deltaTime * speed);
+            }
+            if (transform.position.x < Player.transform.position.x)
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+            }
+        }
 
-        
-        Debug.Log(Control.transform.position);
-        if (transform.position.x > Control.transform.position.x)
+        if (Mathf.Abs((transform.position.y) - (Player.transform.position.y)) > 1.25 || Mathf.Abs((Player.transform.position.y) - (transform.position.y)) > 1.25)
         {
-            transform.Translate(Vector2.left * Time.deltaTime * speed);
-        }
-        if (transform.position.x < Control.transform.position.x)
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * speed);
-        }
-        if (transform.position.y < Control.transform.position.y)
-        {
-            transform.Translate(Vector2.up * Time.deltaTime * speed);
-        }
-        if (transform.position.y > Control.transform.position.y)
-        {
-            transform.Translate(Vector2.down * Time.deltaTime * speed);
+            if (transform.position.y < Player.transform.position.y)
+            {
+                transform.Translate(Vector2.up * Time.deltaTime * speed);
+            }
+            if (transform.position.y > Player.transform.position.y)
+            {
+                transform.Translate(Vector2.down * Time.deltaTime * speed);
+            }
         }
     }
 }
