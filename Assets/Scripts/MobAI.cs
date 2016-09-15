e5b4aae58546a8963a33bb68c7b8be9691d8d285
unknown <bjdenys@mtu.edu>
2016-09-15 11:56:46 -0400

    added aggro range to mobs

--- Assets/Scripts/MobAI.cs
+++ Assets/Scripts/MobAI.cs
@@ -3,39 +3,57 @@ using System.Collections;
 
 public class MobAI : MonoBehaviour {
     public GameObject Player;
-    int MinDist = 5;
-    float speed = 2;
-	// Use this for initialization
-	void Start () {
+    float aggroRange = 5;    //Distance when the unit will start tracking the player  
+    float deaggroRange = 10;  //Distance when the unit will stop tracking the player
+    bool aggro = false;       //If the unit is tracking the player
+    double minDist = 1.25;    //If the player is less than minDist away the unit will stop moving closer
+    float baseSpeed = 2;      //The unit's base speed
+    float speed = 2;          //Value that changes when buffed or debuffed
+    float health = 10;        //The unit's current health
+    // Use this for initialization
+    void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log((transform.position.x) - (Player.transform.position.x));
-        Debug.Log((Player.transform.position.x) - (transform.position.x));
-        if (Mathf.Abs((transform.position.x) - (Player.transform.position.x)) > 1.25|| Mathf.Abs((Player.transform.position.x) -(transform.position.x)) > 1.25)
-        {
-            if (transform.position.x > Player.transform.position.x)
-            {
-                transform.Translate(Vector2.left * Time.deltaTime * speed);
-            }
-            if (transform.position.x < Player.transform.position.x)
-            {
-                transform.Translate(Vector2.right * Time.deltaTime * speed);
-            }
-        }
 
-        if (Mathf.Abs((transform.position.y) - (Player.transform.position.y)) > 1.25 || Mathf.Abs((Player.transform.position.y) - (transform.position.y)) > 1.25)
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Player.transform.position) < aggroRange)
         {
-            if (transform.position.y < Player.transform.position.y)
+            aggro = true;
+        }
+        if(Vector2.Distance(transform.position, Player.transform.position) > deaggroRange)
+        {
+            aggro = false;
+        }
+        if (aggro)
+        {
+            // determine's if the mob is close enough to stop moving along the x-axis
+            if (Vector2.Distance(transform.position, Player.transform.position) > minDist)
             {
-                transform.Translate(Vector2.up * Time.deltaTime * speed);
-            }
-            if (transform.position.y > Player.transform.position.y)
-            {
-                transform.Translate(Vector2.down * Time.deltaTime * speed);
+                if (transform.position.x > Player.transform.position.x)
+                {
+                    transform.Translate(Vector2.left * Time.deltaTime * speed);
+                }
+                if (transform.position.x < Player.transform.position.x)
+                {
+                    transform.Translate(Vector2.right * Time.deltaTime * speed);
+                }
+
+                //determines if the mob is close enough to stop moving along the y-axis
+                if (transform.position.y < Player.transform.position.y)
+                {
+                    transform.Translate(Vector2.up * Time.deltaTime * speed);
+                }
+                if (transform.position.y > Player.transform.position.y)
+                {
+                    transform.Translate(Vector2.down * Time.deltaTime * speed);
+                }
             }
         }
+    }
+    public void mobHealth(int damage)
+    {
+        health = health - damage;
     }
 }
