cea3ec86efc0e639a3376d77dd4be54230d3d5f0
Benjamin Denys <bjdenys@mtu.edu>
2017-02-19 20:25:32 -0500

    Added a main menu

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
