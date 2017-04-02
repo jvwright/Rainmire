a2c9add58ff7d140f91836cf66af132c7844e536
Luteyjam <jace.lutey@gmail.com>
2017-04-02 19:07:02 -0400

    More sound stuff

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,10 +9,23 @@ public class PlayerHealth : MonoBehaviour
     public GameObject image;
     public bool cantTouchThis = false;     //attempting to prevent character from taking so much projectile damage
     float InvinciTimer = 5;
+    public AudioClip sound;
+    private AudioSource hurt;
+
     // Use this for initialization
     void Start()
     {
         HP = 100;
+        player = this.gameObject;
+        if(player != null)
+        {
+            hurt = player.AddComponent<AudioSource>();
+            hurt.clip = sound;
+            hurt.loop = false;
+            hurt.playOnAwake = false;
+            hurt.volume = 1;
+
+        }
     }
 
     // Update is called once per frame
@@ -43,6 +56,9 @@ public class PlayerHealth : MonoBehaviour
                 player.gameObject.SetActive(false);
             }
             GameObject.Find("GreenHealthBar").transform.GetComponent("HealthBar").SendMessage("decreaseHealth", HP);
+            
+                hurt.Play();
+            
         }
     }
 }
